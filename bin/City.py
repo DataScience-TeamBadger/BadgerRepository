26d38c6e834745ef904db666a48e4833b9a03830
Alex Kerzner <akerzner@live.com>
2017-04-24 18:28:01 -0400

    Application, revision b2.0
    - Rewrote code to allow more flexibility in importing new files
    - Normalized CSV data
    - Added type-parsing for some fields

--- bin/City.py
+++ bin/City.py
@@ -4,82 +4,102 @@
 Created on Sun Apr 23 18:51:58 2017
 @author: jd
 """
+# CSV file parsing
+import csv
+
+from datetime import datetime
+
+# TODO: import either fastkml or pykml for kml files?
+
+
+
 import numpy as np
 from models import Scatter_Model,Ridership_Model,Budget_Model
 
 class City(object):
-    
-    metro_time = []
-    metro_ridership = []
-    metro_budget = []
-    metro_coverage = []
-    
-    bus_time = []
-    bus_ridership = []
-    bus_budget = []
-    bus_coverage = []
-    
-    def __init__(self, cityname, metro_csv, bus_csv):#, metro_coverage, bus_coverage):
-        #self._id = cityid
-        self.name = cityname
-        self._mcsv = metro_csv
-        self._bcsv = bus_csv
-        self.metro_parseCSV()
-        self.bus_parseCSV()
-        self.models = {}
-        self.model_names = []
-        self.createModels()
-
-    # parsecsvintonparrays()
-    # createModel(np1, np2)
-    
-    def bus_parseCSV(self):
-        f = open(self._mcsv)
-        f.readline()
-        for line in f:
-            splitline = line.split(",")
-            self.bus_time.append(splitline[0])
-            self.bus_ridership.append(splitline[1])
-            self.bus_budget.append(splitline[2])
-            self.bus_coverage.append(splitline[3])
-        f.close()
-    
-    def metro_parseCSV(self):
-        f = open(self._bcsv)
-        f.readline()
-        for line in f:
-            splitline = line.split(",")
-            self.metro_time.append(splitline[0])
-            self.metro_ridership.append(splitline[1])
-            self.metro_budget.append(splitline[2])
-            self.metro_coverage.append(splitline[3])
-        f.close()
-    
-    """""
-    JD I want you to follow the examples below and mimic the creation of the models below for each of the model names given.
-    Steps:
-    1. Give the model a name(Did that for you)
-    2. Append the model name to the model_names list
-    3. Make an elemen in the models dictionary as this example shows Model_Class.Model_Class(model_name, x_variable_array, y_variable_array)
-        a. The Model_Class refers to the model class you will be making. named something like metro_bus_ridership_model.py for the first model
-        b. So for each model name I put down add your model class and it should show up. In order to make the model class make sure it fits the constructor and fits the Qt API, Alex put some help on the slack in #samplecode
-    """""
-    def createModels(self):
-        model_name = "Metro vs Bus (Ridership)" #Bar
-        self.model_names.append(model_name)
-        self.models[model_name] = Ridership_Model.Ridership_Model(model_name,self.metro_ridership, self.bus_ridership)
-        model_name = "Metro vs Bus (Budget)" #Bar
-        self.model_names.append(model_name)
-        self.models[model_name] = Budget_Model.Budget_Model(model_name,self.metro_budget, self.bus_budget)
-        model_name = "Metro vs Bus (Coverage)" #Map Leave this one to me and Steven(This may also be two seperate ones but I imagined plotting each point in one color for bus stations and another for metro stations, and then overtop a transparant color that covers coverage area)
-        model_name = "Metro Ridership vs Time" #Scatter w/ LinRegression
-        model_name = "Bus Ridership vs Time"    #Scatter w/ LinRegression
-        model_name = "Metro Ridership vs Coverage(km2)" #Scatter w/ LinRegression
-        model_name = "Bus Ridership vs Coverage(km2)" #Scatter w/ LinRegression
-
-    # unnecessary - city.model_names from outside
-    def get_model_names(self):
-        return self.model_names
-    # unnecessary - city.models from outside.
-    def get_models(self):
-        return self.models
+	
+	# Format for date as parsed from CSV files
+	DATE_FORMAT = "%Y-%m"
+	
+	
+	"""
+	City constructor
+	"""
+	def __init__(self, city_name, path_to_metro_csv, path_to_bus_csv):#, metro_coverage, bus_coverage):
+		self.name = city_name
+		self.models = {}
+		self.model_names = []
+		
+		self.time       = {'metro':[],'bus':[]}
+		self.ridership  = {'metro':[],'bus':[]}
+		self.budget     = {'metro':[],'bus':[]}
+		self.coverage   = {'metro':[],'bus':[]}
+		self.population = {'metro':[],'bus':[]}
+		
+		self._parseCSV('metro',1,path_to_metro_csv)
+		self._parseCSV('bus',1,path_to_bus_csv)
+		self.createModels()
+	
+	"""
+	Parse a given CSV file, given a data source and type.
+	
+	@param data_source = The data source i.e. 'metro','bus'
+	@param data_type = An integer representing which data format is provided.
+	@param path_to_csv = The full path to the CSV file
+	"""
+	def _parseCSV(self, data_source, data_type, path_to_csv):
+		with open(path_to_csv) as csv_file:
+			# Read the file as a csv file
+			reader = csv.DictReader(csv_file)
+			if (data_type == 1):
+				# Parse time, ridership, budget, and coverage
+				for row in reader:
+					# Parse data
+					self.time[data_source].append(datetime.strptime(row["YEAR-MONTH"], self.DATE_FORMAT))
+					self.ridership[data_source].append(int(row["RIDERSHIP"]))
+					self.budget[data_source].append(int(row["BUDGET"]))
+					self.coverage[data_source].append(row["COVERAGE"])
+					self.population[data_source].append(row["POPULATION"])
+			elif (data_type == 2):
+				# Another data type
+				# Parse this data type
+				return
+	
+	"""""
+	JD I want you to follow the examples below and mimic the creation of the models below for each of the model names given.
+	Steps:
+	1. Give the model a name(Did that for you)
+	2. Append the model name to the model_names list
+	3. Make an elemen in the models dictionary as this example shows Model_Class.Model_Class(model_name, x_variable_array, y_variable_array)
+		a. The Model_Class refers to the model class you will be making. named something like metro_bus_ridership_model.py for the first model
+		b. So for each model name I put down add your model class and it should show up. In order to make the model class make sure it fits the constructor and fits the Qt API, Alex put some help on the slack in #samplecode
+	"""""
+	def createModels(self):
+		
+		# Bar
+		model_name = "Metro vs Bus (Ridership)"
+		self.model_names.append(model_name)
+		self.models[model_name] = Ridership_Model.Ridership_Model(model_name, self.ridership['metro'], self.ridership['bus'])
+		
+		# Bar
+		model_name = "Metro vs Bus (Budget)"
+		self.model_names.append(model_name)
+		self.models[model_name] = Budget_Model.Budget_Model(model_name, self.budget['metro'], self.budget['bus'])
+		
+		# Map - Leave this one to me and Steven
+		# (This may also be two seperate ones but I imagined plotting each point 
+		# in one color for bus stations and another for metro stations, and then
+		# overtop a transparant color that covers coverage area)
+		model_name = "Metro vs Bus (Coverage)"
+		
+		# Scatter w/ LinRegression
+		model_name = "Metro Ridership vs Time"
+		
+		# Scatter w/ LinRegression
+		model_name = "Bus Ridership vs Time"
+		
+		# Scatter w/ LinRegression
+		model_name = "Metro Ridership vs Coverage(km2)"
+		
+		# Scatter w/ LinRegression
+		model_name = "Bus Ridership vs Coverage(km2)"
